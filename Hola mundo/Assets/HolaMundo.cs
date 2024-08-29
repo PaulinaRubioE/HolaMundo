ae3414b0b63a037c42f22781d9f49534be38e58f
PaulinaRubioE <rpaulinaelizabeth@gmail.com>
2024-08-28 22:19:10 -0600

    hola

---
+++ Hola mundo/Assets/HolaMundo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() => print("Hola Mundo, soy Paulina");
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
